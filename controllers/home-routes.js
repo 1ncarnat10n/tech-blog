b53c5a5f856aae1c821a85db8a6a1dbd3fe46b69
BasedAtlas <tsaiwilliam888@gmail.com>
2023-11-28 16:08:38 -0800

    Updated handlebars and added js files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {User, Post, Comment} = require('../models');
+const withAuth = require('../utils/auth');
 
 // Route to render homepage
 router.get("/", async (req, res) => {
@@ -20,12 +21,31 @@ router.get("/", async (req, res) => {
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  // if (req.session.logged_in) {
-  //   res.redirect('/profile');
-  //   return;
-  // }
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
 
   res.render('login');
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
