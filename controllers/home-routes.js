11c385d99477146db60272327cd272b0f1b21d17
BasedAtlas <tsaiwilliam888@gmail.com>
2023-11-28 18:46:42 -0800

    Added new routes for posts and comments, updated handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
-const {User, Post, Comment} = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // Route to render homepage
 router.get("/", async (req, res) => {
-    try {
-      const postData = await Post.findAll({
-        include: [{ model: User, attributes: ["name"] }],
-      });
-      const posts = postData.map((post) => post.get({ plain: true }));
-      // Render homepage template with posts and login status
-      res.render("homepage", {
-        posts,
-        logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User, attributes: ["name"] }],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    // Render homepage template with posts and login status
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
@@ -46,6 +46,43 @@ router.get('/dashboard', withAuth, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+//Route to get single post 
+router.get("/post/:id", withAuth, async (req, res) => {
+  try {
+    // Find post by ID with associated username and comments with associated usernames
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        { model: User, attributes: ["name"] },
+        {
+          model: Comment,
+          include: [{ model: User, attributes: ["name"] }],
+        },
+      ],
+    });
+    // Convert post data to plain JavaScript object
+    const post = postData.get({ plain: true });
+    // Render post template with post data and login status
+    res.render("post", {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    // If there is an error, return 500 status code and error message
+    res.status(500).json(err);
+  }
+});
+
+//route to go to newPostForm handlebar 
+router.get("/newpost", withAuth, (req, res) => {
+  if (req.session.logged_in) {
+    res.render("newPostForm", {
+      logged_in: req.session.logged_in,
+    });
+    return;
+  }
+  //res.redirect("/logout");
 });
 
 module.exports = router;
