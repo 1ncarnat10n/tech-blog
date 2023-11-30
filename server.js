6e9eea489e0e62fa0cb61fa505156eae20cf8ed7
BasedAtlas <tsaiwilliam888@gmail.com>
2023-11-29 19:45:41 -0800

    Trying to fix connection again

--- server.js
+++ server.js
@@ -18,9 +18,6 @@ const sess = {
   secret: 'Super secret secret',
   cookie: {
     maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
   },
   resave: false,
   saveUninitialized: true,
